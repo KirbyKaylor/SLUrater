f60d4e52f4df402981176fa7e18afa136125d371
Karen Mullen <kemull10@stlawu.edu>
2014-04-14 16:27:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+// Server for SLUrater
 var express = require('express');
 var app = express();
 
-app.get('/', require('./routes/index'));
+// Configure the server
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/views');
+app.use(express.urlencoded({limit:'1kb'}));
+app.use(express.static(__dirname+'/statics'));
 
-app.get('*', require('./routes/default'));
+// Enable sessions
+app.use(express.cookieParser());
+app.use(express.session({secret:'This session'}));
 
+// Route the requests
+//app.get('/', require('./routes/'));
+app.post('/register', require('./routes/register'));
+app.post('/login', require('./routes/login'));
+//app.get('/logout', require('./routes/logout'));
+//app.get('*', require('./routes/default'));
+
+// Default route
+app.get('*', function(request,response) {
+    response.send('Nothing to see here!');
+});
+
+// Start the server
 app.listen(8080);
 console.log('Server is up.');
