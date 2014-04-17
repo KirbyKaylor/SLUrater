8fcd6648d8e9c0ccb70290c4fb7a04d121beed16
Sarah Koallick <smkoal10@gmail.com>
2014-04-17 13:09:03 -0400

    The dining services route and view.

---
+++ routes/dining.js
@@ -0,0 +1,10 @@
+// Dining services route
+
+module.exports = function(request,response) {
+    //
+    //var username = request.session.username;
+    //
+    //if (username) {
+        response.render('dining')
+
+};
