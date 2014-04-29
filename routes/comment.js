2289761682b7c4dc5283381b9e50bb0a44a1d70d
tquinlan1992@gmail.com <tequin10@CS-340-09.stlawu.local>
2014-04-29 13:50:36 -0400

    Made changes to routes/comment to accomodate for category page specificity

--- routes/comment.js
+++ routes/comment.js
@@ -4,11 +4,13 @@ var validator = require('validator');
 module.exports = function(request, response) {
     var comment = validator.escape(request.body.comment);
     var pageid = request.body.pageid;
+    var categories = ["diningpage", "housingpage", "buildingspage", "coursespage", "events"];
+    var category = categories[request.body.category];
     var username = request.body.username
     var url = request.url;
     
 // CHECK TO BE SURE THAT THIS DOESN'T NEED THE MONGO.BLAH CHANGES THAT LISA MADE    
     comments.create(comment, pageid, username, function() {
-        response.redirect('diningpage/'+pageid);    
+        response.redirect(category + '/' + pageid);    
     });
 };
