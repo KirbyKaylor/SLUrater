c64f63cb0ebbbf11fec53ef863a9edb83fce7ca5
tquinlan1992@gmail.com <tequin10@CS-340-09.stlawu.local>
2014-04-29 13:24:25 -0400

    fixed rate so it can be translated to the other pages

--- routes/rate.js
+++ routes/rate.js
@@ -3,11 +3,13 @@ var ratings = require('../models/ratings');
 module.exports = function(request,response) {
     var pageid = request.body.pageid;
     var url = request.url;
+    var categories = ["diningpage", "housingpage", "buildingspage", "coursespage", "events"];
     var index = url.lastIndexOf("/");
-    var like = url.substring(index+1,index+2);
-    var pageid = url.substring(index+2);
+    category = categories[url.substring(index+1, index+2)];
+    var like = url.substring(index+2,index+3);
+    var pageid = url.substring(index+3);
     
     ratings.rate(pageid, like, function() {
-        response.redirect('diningpage/'+pageid);
+        response.redirect(category + '/' + pageid);
     });
 };
