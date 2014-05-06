ba53d8a95fc7d6967e6fabe84244f797a346421e
tquinlan1992@gmail.com <tequin10@CS-340-09.stlawu.local>
2014-05-06 17:35:24 -0400

    Set up one admin page, can change it so only if you're an admin do you see it in nav

--- routes/adminpage.js
+++ routes/adminpage.js
@@ -1,3 +1,4 @@
+var comments = require('../models/comments');
 var users = require('../models/users');
 
 module.exports = function(request,response) {
@@ -5,7 +6,15 @@ module.exports = function(request,response) {
     var username = request.session.username;
     users.retrieveUsers(function(usersView) {
         users.findUser(username, function (user) {
-            response.render('users',{users:usersView, session:user})
+            comments.retrieveAllComments(function(comments) {
+                response.render('adminpage',{comments:comments, users:usersView, session:user});
+            });
         });
     });
+    
+    
+
+
 };
+
+    
