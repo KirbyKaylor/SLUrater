12941724a279a72baaa69dcb3ab091dfba63317d
tquinlan1992@gmail.com <tequin10@CS-340-09.stlawu.local>
2014-05-06 16:32:53 -0400

    No create page if you're not logged in

--- routes/housing.js
+++ routes/housing.js
@@ -1,7 +1,8 @@
 var pages = require('../models/pages');
 
 module.exports = function(request,response) {
+    var username = request.session.username;
     pages.retrievePages("housing", function(page) {
-        response.render('housing',{house:page});
+        response.render('housing',{house:page, username:username});
     });
 };
