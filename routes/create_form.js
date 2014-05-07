6607321080f44117d692f3e33c0d27365edb01b8
tquinlan1992@gmail.com <tequin10@CS-340-09.stlawu.local>
2014-05-06 20:13:25 -0400

    Can only view create a page if you are logged in

--- routes/create_form.js
+++ routes/create_form.js
@@ -1,8 +1,8 @@
 // Route to the form for creating a page
 
 module.exports = function(request,response) {
-
+var username = request.session.username;
 // need to get the item id number
-        response.render('create_form')
+        response.render('create_form', {username:username})
 
 };
